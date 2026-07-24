63369da5b71f669bf6deec594683c462f75fa060
vartika <118068612+vartika111@users.noreply.github.com>
2026-07-24 14:54:39 +0530

    Time: 497 ms (40.27%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+with cte as
+(select customer_number,count(order_number) as NumOrd
+from Orders 
+group by customer_number)
+
+select customer_number
+from cte
+where NumOrd= (select max(NumOrd) from cte)
