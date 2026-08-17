e822dca3ad486cd8066106ab83e9de9027626850
vartika <118068612+vartika111@users.noreply.github.com>
2026-08-17 19:15:29 +0530

    Time: 4 ms (20.57%), Space: 63 MB (49.53%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            
+            int width = right - left;
+            
+            int h = min(height[left], height[right]);
+            
+            int area = width * h;
+            
+            maxArea = max(maxArea, area);
+
+            // Move the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+};
